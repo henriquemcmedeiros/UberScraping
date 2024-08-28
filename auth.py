97eea82ba65be7390f2d098b7c979af0554e6f55
Henrique Medeiros <84346920+henriquemcmedeiros@users.noreply.github.com>
2024-08-28 18:37:03 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -8,23 +8,17 @@ def get_csrf_token():
         page = context.new_page()
         page.goto('https://auth.uber.com/login/')
         
-        # Preencher o campo de email/telefone usando o id
         page.fill('input[name="email"]', 'piwebscrapping@gmail.com')
 
-        # Clicar no botão "Next" (ou "Submit")
         page.click('button[type="submit"]')
 
-        # Esperar o campo de senha aparecer e preenchê-lo
         page.wait_for_selector('input[type="password"]')
         page.fill('input[type="password"]', 'piteste01')
 
-        # Clicar no botão "Submit" após preencher a senha
         page.click('button[type="submit"]')
 
-        # Esperar a página carregar completamente
         page.wait_for_navigation()
 
-        # Esperar a página carregar e o token estar disponível
         try:
             page.wait_for_selector('meta[name="csrf-token"]', state='visible', timeout=10000)
             csrf_token = page.get_attribute('meta[name="csrf-token"]', 'content')
@@ -32,7 +26,6 @@ def get_csrf_token():
         except Exception as e:
             print(f"Erro: {e}")
         
-        # Fechar o navegador
         browser.close()
 
 if __name__ == "__main__":
