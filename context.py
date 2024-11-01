12ae787617694b93ac9c3e1f0d169e2812a7e521
Henrique Medeiros <84346920+henriquemcmedeiros@users.noreply.github.com>
2024-11-01 15:47:12 -0300

    Update context.py

--- context.py
+++ context.py
@@ -1,24 +1,47 @@
+import subprocess
 import pyodbc
 import os
+import platform
 from dotenv import load_dotenv
+
+def install_odbc_driver():
+    system = platform.system()
+    
+    if system == "Linux":
+        # Example for Ubuntu/Debian-based systems
+        print("Checking and installing ODBC driver on Linux...")
+        subprocess.run("sudo apt-get update", shell=True)
+        subprocess.run("sudo apt-get install -y unixodbc-dev", shell=True)
+        subprocess.run("sudo apt-get install -y msodbcsql17", shell=True)
+
 
 # Carregar variáveis de ambiente
 load_dotenv()
 
-DB_SERVER= "thyagoquintas.com.br"
-DB_NAME= "uber"
-DB_USER= "uber"
-DB_PASSWORD= "uber"
+install_odbc_driver()
 
 class Database:
     def __init__(self):
+        if platform.system() == "Windows":
+            driver = "SQL Server"
+        else: 
+            
+            driver = "ODBC Driver 17 for SQL Server"  # Ou "ODBC Driver 18 for SQL Server" se este estiver instalado
+
+        print("DB_SERVER:", os.getenv("DB_SERVER"))
+        print("DB_NAME:", os.getenv("DB_NAME"))
+        print("DB_USER:", os.getenv("DB_USER"))
+        print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
+        print("DRIVER:", driver)
+
+        # Conectar ao banco de dados com o driver adequado
         self.conn = pyodbc.connect(
-            f'DRIVER={{SQL Server}};'
-            f'SERVER={DB_SERVER};'
-            f'DATABASE={DB_NAME};'
-            f'UID={DB_USER};'
-            f'PWD={DB_PASSWORD};'
-            f'TIMEOUT=120'  # Aumenta o tempo limite para 120 segundos
+            f'DRIVER={{{driver}}};'
+            f'SERVER={os.getenv("DB_SERVER")};'
+            f'DATABASE={os.getenv("DB_NAME")};'
+            f'UID={os.getenv("DB_USER")};'
+            f'PWD={os.getenv("DB_PASSWORD")};'
+            f'TIMEOUT=120'
         )
 
         self.cursor = self.conn.cursor()
